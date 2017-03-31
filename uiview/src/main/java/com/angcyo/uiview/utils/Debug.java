8aaf425a207e1c0731a21f5fc8cf8704cd9f78a7
angcyo <angcyo@126.com>
2017-03-31 15:37:21 +0800

    add debug util

--- uiview/src/main/java/com/angcyo/uiview/utils/Debug.java
+++ uiview/src/main/java/com/angcyo/uiview/utils/Debug.java
@@ -1,5 +1,9 @@
 package com.angcyo.uiview.utils;
 
+import com.angcyo.library.utils.L;
+
+import java.util.Stack;
+
 /**
  * Copyright (C) 2016,深圳市红鸟网络科技股份有限公司 All rights reserved.
  * 项目名称：
@@ -12,4 +16,18 @@ package com.angcyo.uiview.utils;
  * Version: 1.0.0
  */
 public class Debug {
+
+    private static Stack<Long> sStartTime = new Stack<>();
+
+    public static void logTimeStart(String log) {
+        long time = System.currentTimeMillis();
+        sStartTime.push(time);
+        L.e(log + " --start:" + time);
+    }
+
+    public static void logTimeEnd(String log) {
+        final long endTime = System.currentTimeMillis();
+        Long pop = sStartTime.pop();
+        L.e(log + " ----end:" + (endTime - pop) / 1000 + "秒" + (endTime - pop) % 1000);
+    }
 }
