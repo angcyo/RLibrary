5a1d63e7bd592801b85cb77008fff45cf71693b0
angcyo <angcyo@126.com>
2017-10-24 14:49:14 +0800

    fix base

--- library/src/main/java/com/angcyo/library/utils/Anim.java
+++ library/src/main/java/com/angcyo/library/utils/Anim.java
@@ -4,6 +4,7 @@ import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPropertyAnimatorCompat;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.EditText;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
@@ -32,6 +33,9 @@ public class Anim {
      */
     public static void band(View target) {
         YoYo.with(Techniques.RubberBand).interpolate(new DecelerateInterpolator()).duration(300).playOn(target);
+        if (target instanceof EditText) {
+            target.requestFocus();
+        }
     }
 
     public static ViewPropertyAnimatorCompat anim(final View target) {
