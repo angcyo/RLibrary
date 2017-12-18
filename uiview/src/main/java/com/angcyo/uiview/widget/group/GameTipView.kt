445c6b79271fbbc2136cc8826e8f2da865b56b5e
angcyo <angcyo@126.com>
2017-12-18 13:52:11 +0800

    add game tip view

--- uiview/src/main/java/com/angcyo/uiview/widget/group/GameTipView.kt
+++ uiview/src/main/java/com/angcyo/uiview/widget/group/GameTipView.kt
@@ -1,10 +1,15 @@
 package com.angcyo.uiview.widget.group
 
+import android.animation.Animator
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.LinearInterpolator
 import android.widget.FrameLayout
 import com.angcyo.uiview.R
+import com.angcyo.uiview.resources.RAnimListener
 import com.angcyo.uiview.widget.RTextView
 
 /**
@@ -23,6 +28,20 @@ class GameTipView(context: Context, attributeSet: AttributeSet? = null) : FrameL
     var tipTextView: RTextView
     var timeTextView: RTextView
 
+    /**是否一直显示小时(否则只在值>0时显示)*/
+    var showHour = false
+
+    /**是否一直显示分钟(否则只在值>0时显示)*/
+    var showMin = false
+
+    /**秒数, 是否显示2位, 自动补齐0*/
+    var twoSecBit = false
+
+    private var fromTime = 0
+
+    /**到时间了*/
+    var onTimeEnd: (() -> Unit)? = null
+
     init {
         View.inflate(context, R.layout.base_game_tip_layout, this)
         tipTextView = findViewById(R.id.tip_text_view)
@@ -38,7 +57,62 @@ class GameTipView(context: Context, attributeSet: AttributeSet? = null) : FrameL
     }
 
     /**开始倒计时*/
-    fun startCountDown(fromTime: Int /*从多少秒开始倒计时*/) {
+    fun startCountDown(fromTime: Int /*从多少秒开始倒计时*/, onEnd: (() -> Unit)? = null) {
         //tipTextView.text = text
+        this.onTimeEnd = onEnd
+        this.fromTime = fromTime
+        setTimeText(formatTime(fromTime * 1000L))
+        startAnim()
+    }
+
+    private fun formatTime(millisecond: Long /*毫秒*/): String {
+        val mill = millisecond / 1000
+
+        val min = mill / 60
+        val hour = min / 60
+        val second = mill % 60
+
+        val builder = StringBuilder()
+        if (hour > 0 || showHour) {
+            builder.append(if (hour >= 10) hour else "0" + hour)
+            builder.append(":")
+        }
+        if (min > 0 || showMin) {
+            builder.append(if (min >= 10) min else "0" + min)
+            builder.append(":")
+        }
+        builder.append(if (second >= 10 || !twoSecBit) second else "0" + second)
+
+        return builder.toString()
+    }
+
+    private var timeAnim: ValueAnimator? = null
+    private fun startAnim() {
+        stopAnim()
+        timeAnim = ObjectAnimator.ofInt(fromTime, 0).apply {
+            duration = fromTime * 1000L
+            interpolator = LinearInterpolator()
+            addUpdateListener {
+                setTimeText(formatTime((animatedValue as Int) * 1000L))
+            }
+            addListener(object : RAnimListener() {
+                override fun onAnimationFinish(animation: Animator?, cancel: Boolean) {
+                    super.onAnimationFinish(animation, cancel)
+                    if (!cancel) {
+                        onTimeEnd?.invoke()
+                    }
+                }
+            })
+            start()
+        }
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        stopAnim()
+    }
+
+    fun stopAnim() {
+        timeAnim?.cancel()
     }
 }
