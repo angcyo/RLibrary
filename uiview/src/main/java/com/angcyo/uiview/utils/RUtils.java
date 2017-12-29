1f843600ce22699cda0fea5954548adda0ef5516
angcyo <angcyo@126.com>
2017-12-29 19:02:45 +0800

    fix base

--- uiview/src/main/java/com/angcyo/uiview/utils/RUtils.java
+++ uiview/src/main/java/com/angcyo/uiview/utils/RUtils.java
@@ -1479,7 +1479,8 @@ public class RUtils {
                 parrern.append(".");
                 parrern.append(bitNum);
                 parrern.append("f");
-                return String.format(Locale.CHINA, parrern.toString(), value);
+                return String.format(Locale.CHINA, parrern.toString(), ((int) (Math.pow(10, bitNum) * value)) / Math.pow(10, bitNum));
+//                return String.valueOf(((int) (Math.pow(10, bitNum) * value)) / Math.pow(10, bitNum));
             } else {
                 return String.format(Locale.CHINA, "%s", value);
             }
