904b6e7df44a3c3b366eff5c36abf6d78e7bf10f
angcyo <angcyo@126.com>
2018-01-27 09:13:29 +0800

    fix base

--- library/src/main/java/com/angcyo/library/utils/RIo.java
+++ library/src/main/java/com/angcyo/library/utils/RIo.java
@@ -1,12 +1,18 @@
 package com.angcyo.library.utils;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 
+import okio.Buffer;
 import okio.BufferedSink;
 import okio.BufferedSource;
+import okio.ByteString;
 import okio.Okio;
 import okio.Sink;
 import okio.Source;
@@ -23,6 +29,8 @@ import okio.Source;
  * Version: 1.0.0
  */
 public class RIo {
+
+    private static final ByteString PNG_HEADER = ByteString.decodeHex("89504e470d0a1a0a");
 
     public static long copyFile(String fromFilePath, String toFilePath) throws FileNotFoundException {
         return copyFile(new File(fromFilePath), toFilePath);
@@ -105,5 +113,38 @@ public class RIo {
             }
         }
         return false;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public static void decodePng(InputStream in) throws IOException {
+        try (BufferedSource pngSource = Okio.buffer(Okio.source(in))) {
+            ByteString header = pngSource.readByteString(PNG_HEADER.size());
+            if (!header.equals(PNG_HEADER)) {
+                throw new IOException("Not a PNG.");
+            }
+
+            while (true) {
+                Buffer chunk = new Buffer();
+
+                // Each chunk is a length, type, data, and CRC offset.
+                int length = pngSource.readInt();
+                String type = pngSource.readUtf8(4);
+                pngSource.readFully(chunk, length);
+                int crc = pngSource.readInt();
+
+                decodeChunk(type, chunk);
+                if (type.equals("IEND")) break;
+            }
+        }
+    }
+
+    private static void decodeChunk(String type, Buffer chunk) {
+        if (type.equals("IHDR")) {
+            int width = chunk.readInt();
+            int height = chunk.readInt();
+            System.out.printf("%08x: %s %d x %d%n", chunk.size(), type, width, height);
+        } else {
+            System.out.printf("%08x: %s%n", chunk.size(), type);
+        }
     }
 }
