2270a812c2139de46aed716163f3dc0e43490d15
angcyo <angcyo@126.com>
2018-02-26 17:08:33 +0800

    fix base dialog

--- uiview/src/main/java/com/angcyo/uiview/dialog/UIRecyclerDialog.kt
+++ uiview/src/main/java/com/angcyo/uiview/dialog/UIRecyclerDialog.kt
@@ -8,6 +8,7 @@ import com.angcyo.uiview.base.UIIDialogImpl
 import com.angcyo.uiview.recycler.RRecyclerView
 import com.angcyo.uiview.recycler.adapter.RExBaseAdapter
 import com.angcyo.uiview.rsen.RefreshLayout
+import com.angcyo.uiview.widget.RTextView
 
 /**
  * Created by angcyo on 2017-07-23.
@@ -15,7 +16,10 @@ import com.angcyo.uiview.rsen.RefreshLayout
 abstract class UIRecyclerDialog<H, T, F> : UIIDialogImpl() {
     lateinit var refreshLayout: RefreshLayout
     lateinit var recyclerView: RRecyclerView
+    lateinit var titleView: RTextView
     lateinit var adapter: RExBaseAdapter<H, T, F>
+
+    var titleString = ""
 
     override fun inflateDialogView(dialogRootLayout: FrameLayout, inflater: LayoutInflater): View {
         return inflate(R.layout.base_recycler_dialog_layout)
@@ -25,8 +29,17 @@ abstract class UIRecyclerDialog<H, T, F> : UIIDialogImpl() {
         super.initDialogContentView()
         refreshLayout = mViewHolder.v(R.id.base_refresh_view)
         recyclerView = mViewHolder.v(R.id.base_recycler_view)
+        titleView = mViewHolder.v(R.id.base_title_view)
 
         adapter = createAdapter()
+
+        if (titleString.isNotEmpty()) {
+            titleView.apply {
+                visibility = View.VISIBLE
+                text = titleString
+            }
+        }
+
         initRefreshLayout()
         initRecyclerView()
     }
