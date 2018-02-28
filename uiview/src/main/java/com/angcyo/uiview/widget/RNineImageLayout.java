8adb456cc2afdfdc9a10d586c235ba9132e22a8b
angcyo <angcyo@126.com>
2018-02-28 11:53:38 +0800

    fix base layout, style

--- uiview/src/main/java/com/angcyo/uiview/widget/RNineImageLayout.java
+++ uiview/src/main/java/com/angcyo/uiview/widget/RNineImageLayout.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Shader;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
@@ -14,6 +15,7 @@ import android.widget.RelativeLayout;
 
 import com.angcyo.library.utils.L;
 import com.angcyo.uiview.R;
+import com.angcyo.uiview.utils.RUtils;
 import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
@@ -36,6 +38,7 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
      * 需要加载的图片列表
      */
     List<String> mImagesList = new ArrayList<>();
+    List<INineItemData> mImagesItems = new ArrayList<>();
     /**
      * 用来显示图片的ImageView
      */
@@ -57,6 +60,11 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
     private int mRPaddingTop = 0;
     private int mRPaddingBottom = 0;
 
+    /**
+     * 每一行图片的数量, 如果大于零表示, 强制每一行显示多少个图片, 否则自动计算
+     */
+    private int lineImageCount = 0;
+
     public RNineImageLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         space *= getResources().getDisplayMetrics().density;
@@ -66,6 +74,7 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
         mRPaddingLeft = typedArray.getDimensionPixelOffset(R.styleable.RNineImageLayout_r_paddingLeft, 0);
         mRPaddingRight = typedArray.getDimensionPixelOffset(R.styleable.RNineImageLayout_r_paddingRight, 0);
         mRPaddingTop = typedArray.getDimensionPixelOffset(R.styleable.RNineImageLayout_r_paddingTop, 0);
+        lineImageCount = typedArray.getInt(R.styleable.RNineImageLayout_r_line_image_count, lineImageCount);
         typedArray.recycle();
     }
 
@@ -96,32 +105,32 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
             setMeasuredDimension(0, 0);//没有图片, 设置0大小
         } else if (mNineImageConfig == null) {
             L.w("need set nine image config.");
-            mImageViews.get(0).measure(getSize(measureWidth), getSize(measureWidth));
+            getChildAt(0).measure(getSize(measureWidth), getSize(measureWidth));
             setMeasuredDimension(measureWidth, measureWidth);
         } else {
             final int size = getImageSize();
             int width = measureWidth;
             int height;//需要计算布局的高度
-            if (size == 1) {
+            if (size == 1 && lineImageCount <= 0) {
                 //一张图片
                 final int[] widthHeight = mNineImageConfig.getWidthHeight(1);
                 if (widthHeight[0] == -1) {
                     if (widthHeight[1] > 0) {
-                        mImageViews.get(0).measure(getSize(width), getSize(widthHeight[1]));
+                        getChildAt(0).measure(getSize(width), getSize(widthHeight[1]));
                         setMeasuredDimension(width, widthHeight[1]);
                     } else if (widthHeight[1] == -1) {
-                        mImageViews.get(0).measure(getSize(measureWidth), getSize(measureHeight));
+                        getChildAt(0).measure(getSize(measureWidth), getSize(measureHeight));
                         setMeasuredDimension(measureWidth, measureHeight);
                     } else {
-                        mImageViews.get(0).measure(getSize(width), getSize(width));
+                        getChildAt(0).measure(getSize(width), getSize(width));
                         setMeasuredDimension(width, width);
                     }
                 } else if (widthHeight[0] == 0) {
                     int defaultSize = (int) (getResources().getDisplayMetrics().density * 150);//为0时, 采用默认大小
-                    mImageViews.get(0).measure(getSize(defaultSize), getSize(defaultSize));
+                    getChildAt(0).measure(getSize(defaultSize), getSize(defaultSize));
                     setMeasuredDimension(defaultSize, defaultSize);
                 } else {
-                    mImageViews.get(0).measure(isContainVideo ? getSize(widthHeight[0]) :
+                    getChildAt(0).measure(isContainVideo ? getSize(widthHeight[0]) :
                                     getSize(Math.min(widthHeight[0], measureWidth - mRPaddingRight)),
                             getSize(widthHeight[1]));
                     setMeasuredDimension(isContainVideo ? widthHeight[0] : widthHeight[0] + mRPaddingRight, widthHeight[1]);
@@ -138,8 +147,12 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
                 height = rows * itemWidth + Math.max(0, rows - 1) * space;
 
                 //L.e("call: onMeasure -> " + getImageSize() + " itemSize:" + itemWidth);
-                for (View view : mImageViews) {
-                    view.measure(getSize(itemWidth), getSize(itemWidth));
+//                for (View view : mImageViews) {
+//                    view.measure(getSize(itemWidth), getSize(itemWidth));
+//                }
+
+                for (int i = 0; i < getChildCount(); i++) {
+                    getChildAt(i).measure(getSize(itemWidth), getSize(itemWidth));
                 }
                 setMeasuredDimension(measureWidth, height);
             }
@@ -157,7 +170,7 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
             L.w("don't have image list , skip layout.");
         } else if (mNineImageConfig == null) {
             L.w("need set nine image config.");
-            final RImageView firstView = mImageViews.get(0);
+            final View firstView = getChildAt(0);
             firstView.layout(mRPaddingLeft, mRPaddingTop,
                     mRPaddingLeft + getMeasuredWidth(),
                     mRPaddingTop + getMeasuredHeight());
@@ -167,9 +180,13 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
 //            }
         } else {
             final int size = getImageSize();
-            if (size == 1) {
+            final int columns = getColumns(size);
+            final int rows = getRows(size);
+
+            L.e("call: onLayout([changed, left, top, right, bottom])-> 行:" + rows + " 列:" + columns);
+            if (size == 1 && lineImageCount <= 0) {
                 //一张图片
-                final RImageView firstView = mImageViews.get(0);
+                final View firstView = getChildAt(0);
                 firstView.layout(mRPaddingLeft, mRPaddingTop,
                         mRPaddingLeft + firstView.getMeasuredWidth(),
                         mRPaddingTop + firstView.getMeasuredHeight());
@@ -178,9 +195,6 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
 //                    mNineImageConfig.displayImage(firstView, mImagesList.get(0), getMeasuredWidth(), getMeasuredHeight());
 //                }
             } else {
-                final int columns = getColumns(size);
-                final int rows = getRows(size);
-
                 int l;
                 int t;
                 for (int i = 0; i < rows; i++) {
@@ -191,15 +205,15 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
                             break;
                         }
                         //列数
-                        final RImageView imageView = mImageViews.get(index);
-                        final int width = imageView.getMeasuredWidth();
-                        final int height = imageView.getMeasuredHeight();
+                        final View child = getChildAt(index);
+                        final int width = child.getMeasuredWidth();
+                        final int height = child.getMeasuredHeight();
                         l = mRPaddingLeft + j * width + j * space;
                         t = mRPaddingTop + i * height + i * space;
-                        imageView.layout(l, t, l + width, t + height);
+                        child.layout(l, t, l + width, t + height);
 
 //                        if (mNineImageConfig != null) {
-//                            mNineImageConfig.displayImage(imageView, mImagesList.get(index), imageView.getMeasuredWidth(), imageView.getMeasuredHeight());
+//                            mNineImageConfig.displayImage(child, mImagesList.get(index), child.getMeasuredWidth(), child.getMeasuredHeight());
 //                        }
                     }
                 }
@@ -244,8 +258,15 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
 
                 String url = mImagesList.get(i);
                 imageView.setTag(R.id.tag_url, url);
+
+
                 mNineImageConfig.displayImage(imageView, url,
                         imageView.getMeasuredWidth(), imageView.getMeasuredHeight(), getImageSize());
+
+                if (mNineImageConfig instanceof ExNineImageConfig) {
+                    ((ExNineImageConfig) mNineImageConfig).displayItem(getChildAt(i), i, mImagesItems.get(i),
+                            imageView.getMeasuredWidth(), imageView.getMeasuredHeight(), getImageSize());
+                }
             }
         }
     }
@@ -258,6 +279,18 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
         mImagesList.clear();
         if (imagesList != null) {
             mImagesList.addAll(imagesList);
+        }
+        notifyDataChanged();
+    }
+
+    public void setImagesItems(List<INineItemData> items) {
+        mImagesList.clear();
+        mImagesItems.clear();
+        if (!RUtils.isListEmpty(items)) {
+            for (int i = 0; i < items.size(); i++) {
+                mImagesItems.add(i, items.get(i));
+                mImagesList.add(i, items.get(i).getImageUrl());
+            }
         }
         notifyDataChanged();
     }
@@ -292,7 +325,38 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
 //            }
         } else if (newSize < oldSize) {
             for (int i = oldSize - 1; i >= newSize; i--) {
-                removeView(mImageViews.remove(i));
+                GlideImageView view = mImageViews.remove(i);
+
+                if (getChildAt(i) != view) {
+                    removeViewAt(i);
+                } else {
+                    removeView(view);
+                }
+            }
+        }
+
+        for (int i = 0; i < getChildCount(); i++) {
+            View childAt = getChildAt(i);
+            childAt.setTag(R.id.tag_position, i);
+            if (canItemClick) {
+                View imageView = childAt.findViewById(R.id.base_image_view);
+                if (imageView == null) {
+                    childAt.setOnClickListener(this);
+                } else {
+                    imageView.setTag(R.id.tag_position, i);
+                    imageView.setOnClickListener(this);
+                }
+            } else {
+                View imageView = childAt.findViewById(R.id.base_image_view);
+                if (imageView == null) {
+
+                } else {
+                    childAt = imageView;
+                }
+
+                childAt.setOnClickListener(null);
+                childAt.setClickable(false);
+
             }
         }
 
@@ -350,7 +414,7 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
         Glide.with(this).clear(this);
     }
 
-    private int getImageSize() {
+    public int getImageSize() {
         if (mImagesList == null) {
             return 0;
         }
@@ -358,21 +422,31 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
     }
 
     private void createImageView(int i) {
-        GlideImageView imageView = new GlideImageView(getContext(), null);
+        GlideImageView imageView;
+        if (mNineImageConfig instanceof ExNineImageConfig) {
+            imageView = ((ExNineImageConfig) mNineImageConfig).createImageView(this);
+        } else {
+            imageView = new GlideImageView(getContext(), null);
 //            imageView.setBackgroundColor(Color.BLUE);
+        }
         imageView.setTag(R.id.tag_position, i);
         imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
         mImageViews.add(imageView);
-        if (canItemClick) {
-            imageView.setOnClickListener(this);
+
+        if (imageView.getParent() == null) {
+            addView(imageView, i, new LayoutParams(-2, -2));
         }
-        addView(imageView, i, new LayoutParams(-2, -2));
     }
 
     /**
      * 根据图片数量, 返回行数
      */
     private int getRows(int size) {
+        if (lineImageCount > 0) {
+            return size / (lineImageCount + 1) + 1;
+        }
+
         if (size <= 3) {
             return 1;
         }
@@ -389,6 +463,10 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
      * 根据图片数量, 返回列数
      */
     private int getColumns(int size) {
+        if (lineImageCount > 0) {
+            return lineImageCount;
+        }
+
         if (size == 4) {
             return 2;
         }
@@ -400,6 +478,10 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
         if (mNineImageConfig != null) {
             final int position = (int) v.getTag(R.id.tag_position);
             mNineImageConfig.onImageItemClick((GlideImageView) v, mImagesList, mImageViews, position);
+
+            if (mNineImageConfig instanceof ExNineImageConfig) {
+                ((ExNineImageConfig) mNineImageConfig).onImageItemClick(position, mImagesItems.get(position));
+            }
         }
     }
 
@@ -485,5 +567,39 @@ public class RNineImageLayout extends RelativeLayout implements View.OnClickList
         void displayImage(GlideImageView imageView, String url, int width, int height, int imageSize);
 
         void onImageItemClick(GlideImageView imageView, List<String> urlList, List<GlideImageView> imageList, int index);
+    }
+
+    public interface INineItemData {
+        String getImageUrl();
+    }
+
+    public static class ExNineImageConfig implements NineImageConfig {
+
+        @Override
+        public int[] getWidthHeight(int imageSize) {
+            return new int[]{-1, -1};
+        }
+
+        public GlideImageView createImageView(@NonNull RNineImageLayout nineImageLayout) {
+            return new GlideImageView(nineImageLayout.getContext(), null);
+        }
+
+        @Override
+        public void displayImage(GlideImageView imageView, String url, int width, int height, int imageSize) {
+
+        }
+
+        public void displayItem(@NonNull View childAt, int position, INineItemData iNineItemData, int measuredWidth, int measuredHeight, int imageSize) {
+
+        }
+
+        public void onImageItemClick(int position, INineItemData iNineItemData) {
+
+        }
+
+        @Override
+        public void onImageItemClick(GlideImageView imageView, List<String> urlList, List<GlideImageView> imageList, int index) {
+
+        }
     }
 }
