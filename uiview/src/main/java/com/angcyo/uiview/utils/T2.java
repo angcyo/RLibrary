5e03601152c9a4beae5c28d9ab13208ed2633f4e
angcyo <angcyo@126.com>
2018-03-30 15:43:20 +0800

    fix toast bg color

--- uiview/src/main/java/com/angcyo/uiview/utils/T2.java
+++ uiview/src/main/java/com/angcyo/uiview/utils/T2.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -33,9 +34,14 @@ public class T2 {
     public static final int TYPE_OK = 1;
     public static final int TYPE_INFO = 2;
     public static final int TYPE_ERROR = 3;
+    public static boolean SHOW_TOAST_BG_COLOR = true;
     private static Toast toast;
 
     public static void show(Context context, CharSequence charSequence, int type) {
+        show(context, charSequence, type, Gravity.CENTER);
+    }
+
+    public static void show(Context context, CharSequence charSequence, int type, int gravity) {
         View layout;
         if (toast == null) {
             toast = Toast.makeText(context, "", Toast.LENGTH_SHORT);
@@ -53,34 +59,51 @@ public class T2 {
 
         TextView titleView = find(layout, R.id.base_toast_text_view);
         ImageView imageView = find(layout, R.id.base_toast_image_view);
+        LinearLayout contentLayout = find(layout, R.id.base_toast_content_layout);
+        View rootLayout = find(layout, R.id.base_toast_root_layout);
+
+        contentLayout.setGravity(gravity);
 
         if (titleView != null) {
             titleView.setText(charSequence);
         }
         if (imageView != null) {
             if (type == TYPE_NONE) {
+                if (SHOW_TOAST_BG_COLOR) {
+                    rootLayout.setBackgroundResource(R.drawable.base_toast_shape);
+                }
                 imageView.setVisibility(View.GONE);
             } else {
                 imageView.setVisibility(View.VISIBLE);
+
                 switch (type % 4) {
                     case TYPE_OK:
                         imageView.setImageResource(R.drawable.base_ok);
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                            imageView.setImageTintList(ColorStateList.valueOf(SkinHelper.getSkin().getThemeSubColor()));
+
+                        if (SHOW_TOAST_BG_COLOR) {
+                            rootLayout.setBackgroundResource(R.drawable.base_toast_shape_i);
                         }
                         break;
                     case TYPE_INFO:
+                        rootLayout.setBackgroundResource(R.drawable.base_toast_shape_i);
                         imageView.setImageResource(R.drawable.base_info);
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                            imageView.setImageTintList(ColorStateList.valueOf(SkinHelper.getSkin().getThemeSubColor()));
-                        }
                         break;
                     case TYPE_ERROR:
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                            imageView.setImageTintList(ColorStateList.valueOf(Color.parseColor("#d62119")));
-                        }
                         imageView.setImageResource(R.drawable.base_failed_red);
+                        rootLayout.setBackgroundResource(R.drawable.base_toast_shape_w);
                         break;
+                }
+
+                if (SHOW_TOAST_BG_COLOR) {
+                    titleView.setTextColor(Color.WHITE);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        imageView.setImageTintList(ColorStateList.valueOf(Color.WHITE));
+                    }
+                } else {
+                    titleView.setTextColor(context.getResources().getColor(R.color.base_text_color_dark));
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        imageView.setImageTintList(ColorStateList.valueOf(SkinHelper.getSkin().getThemeSubColor()));
+                    }
                 }
             }
         }
--- uiview/src/main/java/com/angcyo/uiview/utils/T_.java
+++ uiview/src/main/java/com/angcyo/uiview/utils/T_.java
@@ -1,5 +1,7 @@
 package com.angcyo.uiview.utils;
 
+import android.view.Gravity;
+
 import com.angcyo.uiview.RApplication;
 
 /**
@@ -13,14 +15,26 @@ public class T_ {
     }
 
     public static void info(final CharSequence text) {
-        T2.show(RApplication.getApp(), text, T2.TYPE_INFO);
+        T2.show(RApplication.getApp(), text, T2.TYPE_INFO, Gravity.CENTER);
     }
 
     public static void ok(final CharSequence text) {
-        T2.show(RApplication.getApp(), text, T2.TYPE_OK);
+        T2.show(RApplication.getApp(), text, T2.TYPE_OK, Gravity.CENTER);
     }
 
     public static void error(final CharSequence text) {
-        T2.show(RApplication.getApp(), text, T2.TYPE_ERROR);
+        T2.show(RApplication.getApp(), text, T2.TYPE_ERROR, Gravity.CENTER);
+    }
+
+    public static void info(final CharSequence text, int gravity) {
+        T2.show(RApplication.getApp(), text, T2.TYPE_INFO, gravity);
+    }
+
+    public static void ok(final CharSequence text, int gravity) {
+        T2.show(RApplication.getApp(), text, T2.TYPE_OK, gravity);
+    }
+
+    public static void error(final CharSequence text, int gravity) {
+        T2.show(RApplication.getApp(), text, T2.TYPE_ERROR, gravity);
     }
 }
