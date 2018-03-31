2a899190c7e7ccc738b5e9e55e1e0c54a6c0c1a2
angcyo <angcyo@126.com>
2018-03-31 21:06:49 +0800

    fix notify util

--- uiview/src/main/java/com/angcyo/uiview/utils/NotifyUtil.java
+++ uiview/src/main/java/com/angcyo/uiview/utils/NotifyUtil.java
@@ -3,6 +3,7 @@ package com.angcyo.uiview.utils;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -37,9 +38,16 @@ public class NotifyUtil {
         this.NOTIFICATION_ID = ID;
         mContext = context;
         // 获取系统服务来初始化对象
-        nm = (NotificationManager) mContext
-                .getSystemService(Activity.NOTIFICATION_SERVICE);
-        cBuilder = new NotificationCompat.Builder(mContext);
+        nm = (NotificationManager) mContext.getSystemService(Activity.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelID = String.valueOf(ID);
+            String channelName = "channel_name";
+            NotificationChannel notificationChannel = new NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_HIGH);
+            nm.createNotificationChannel(notificationChannel);
+            cBuilder = new NotificationCompat.Builder(mContext, channelID);
+        } else {
+            cBuilder = new NotificationCompat.Builder(mContext);
+        }
     }
 
     /**
@@ -77,25 +85,25 @@ public class NotifyUtil {
         cBuilder.setContentText(content);// 设置通知中心中的内容
         cBuilder.setWhen(System.currentTimeMillis());
 
-		/*
+        /*
          * 将AutoCancel设为true后，当你点击通知栏的notification后，它会自动被取消消失,
-		 * 不设置的话点击消息后也不清除，但可以滑动删除
-		 */
+         * 不设置的话点击消息后也不清除，但可以滑动删除
+         */
         cBuilder.setAutoCancel(true);
         // 将Ongoing设为true 那么notification将不能滑动删除
         // notifyBuilder.setOngoing(true);
         /*
          * 从Android4.1开始，可以通过以下方法，设置notification的优先级，
-		 * 优先级越高的，通知排的越靠前，优先级低的，不会在手机最顶部的状态栏显示图标
-		 */
+         * 优先级越高的，通知排的越靠前，优先级低的，不会在手机最顶部的状态栏显示图标
+         */
         cBuilder.setPriority(NotificationCompat.PRIORITY_MAX);
         /*
          * Notification.DEFAULT_ALL：铃声、闪光、震动均系统默认。
-		 * Notification.DEFAULT_SOUND：系统默认铃声。
-		 * Notification.DEFAULT_VIBRATE：系统默认震动。
-		 * Notification.DEFAULT_LIGHTS：系统默认闪光。
-		 * notifyBuilder.setDefaults(Notification.DEFAULT_ALL);
-		 */
+         * Notification.DEFAULT_SOUND：系统默认铃声。
+         * Notification.DEFAULT_VIBRATE：系统默认震动。
+         * Notification.DEFAULT_LIGHTS：系统默认闪光。
+         * notifyBuilder.setDefaults(Notification.DEFAULT_ALL);
+         */
         int defaults = 0;
 
         if (sound) {
@@ -274,8 +282,8 @@ public class NotifyUtil {
         setCompatBuilder(pendingIntent, smallIcon, ticker, title, content, sound, vibrate, lights);
         /*
          * 因为进度条要实时更新通知栏也就说要不断的发送新的提示，所以这里不建议开启通知声音。
-		 * 这里是作为范例，给大家讲解下原理。所以发送通知后会听到多次的通知声音。
-		 */
+         * 这里是作为范例，给大家讲解下原理。所以发送通知后会听到多次的通知声音。
+         */
 
         new Thread(new Runnable() {
             @Override
@@ -372,6 +380,7 @@ public class NotifyUtil {
      * 发送通知
      */
     private void sent() {
+        //cBuilder.setChannelId("1");
         notification = cBuilder.build();
         // 发送该通知
         nm.notify(NOTIFICATION_ID, notification);
