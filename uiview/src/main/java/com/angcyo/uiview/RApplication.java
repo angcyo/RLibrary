dbc14a6c470b368e4555aba273f14b015eed1483
angcyo <angcyo@126.com>
2018-04-09 13:59:34 +0800

    fix base utils

--- uiview/src/main/java/com/angcyo/uiview/RApplication.java
+++ uiview/src/main/java/com/angcyo/uiview/RApplication.java
@@ -1,9 +1,11 @@
 package com.angcyo.uiview;
 
+import android.annotation.SuppressLint;
 import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.multidex.MultiDex;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
@@ -21,8 +23,10 @@ import com.bumptech.glide.Glide;
 import com.orhanobut.hawk.Hawk;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.List;
 
@@ -70,87 +74,26 @@ public class RApplication extends Application {
     /**
      * 获取设备唯一标识码, 需要权限 android.permission.READ_PHONE_STATE
      */
+    @SuppressLint({"MissingPermission", "HardwareIds"})
     public static String getIMEI() {
         String imei = "Unknown";
         try {
-            imei = ((TelephonyManager) getApp()
-                    .getSystemService(Context.TELEPHONY_SERVICE))
-                    .getDeviceId();
+            TelephonyManager telephonyManager = ((TelephonyManager) getApp()
+                    .getSystemService(Context.TELEPHONY_SERVICE));
+
+            if (telephonyManager != null) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    imei = telephonyManager.getImei();
+                } else {
+                    imei = telephonyManager.getDeviceId();
+                }
+            }
             //L.w("call: getIMEI([])-> " + imei);
         } catch (Exception e) {
             e.printStackTrace();
             //L.e("call: getIMEI([])-> " + imei + " " + e.getMessage());
         }
         return imei;
-    }
-
-    /**
-     * 确保只初始化一次
-     */
-    public static boolean isInitOnce(Application application) {
-
-        int pid = android.os.Process.myPid();
-        String processAppName = getAppName(application, pid);
-        // 如果APP启用了远程的service，此application:onCreate会被调用2次
-        // 为了防止环信SDK被初始化2次，加此判断会保证SDK被初始化1次
-        // 默认的APP会在以包名为默认的process name下运行，如果查到的process name不是APP的process name就立即返回
-
-        if (processAppName == null || !processAppName.equalsIgnoreCase(application.getPackageName())) {
-
-            // 则此application::onCreate 是被service 调用的，直接返回
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
-     * 获取进程号对应的进程名
-     *
-     * @param pid 进程号  android.os.Process.myPid()
-     * @return 进程名
-     */
-    private static String getProcessName(int pid) {
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new FileReader("/proc/" + pid + "/cmdline"));
-            String processName = reader.readLine();
-            if (!TextUtils.isEmpty(processName)) {
-                processName = processName.trim();
-            }
-            return processName;
-        } catch (Throwable throwable) {
-            throwable.printStackTrace();
-        } finally {
-            try {
-                if (reader != null) {
-                    reader.close();
-                }
-            } catch (IOException exception) {
-                exception.printStackTrace();
-            }
-        }
-        return null;
-    }
-
-    private static String getAppName(Application application, int pID) {
-        String processName = null;
-        ActivityManager am = (ActivityManager) application.getSystemService(Context.ACTIVITY_SERVICE);
-        List l = am.getRunningAppProcesses();
-        Iterator i = l.iterator();
-        PackageManager pm = application.getPackageManager();
-        while (i.hasNext()) {
-            ActivityManager.RunningAppProcessInfo info = (ActivityManager.RunningAppProcessInfo) (i.next());
-            try {
-                if (info.pid == pID) {
-                    processName = info.processName;
-                    return processName;
-                }
-            } catch (Exception e) {
-                // Log.d("Process", "Error>> :"+ e.toString());
-            }
-        }
-        return processName;
     }
 
     public static RApplication getApp() {
@@ -169,7 +112,7 @@ public class RApplication extends Application {
         super.onCreate();
         app = this;
 
-        if (isInitOnce(this)) {
+        if (Util.isInitOnce(this)) {
 
             Debug.logTimeStart("RApplication 正在初始化:isInitOnce()");
 
@@ -272,5 +215,173 @@ public class RApplication extends Application {
 //            Fresco.getImagePipeline().clearMemoryCaches();
 //        }
         super.onTrimMemory(level);
+    }
+
+    public static class Util {
+
+        /**
+         * 确保只初始化一次
+         */
+        public static boolean isInitOnce(Application application) {
+
+            int pid = android.os.Process.myPid();
+            String processAppName = getAppName(application, pid);
+            // 如果APP启用了远程的service，此application:onCreate会被调用2次
+            // 为了防止环信SDK被初始化2次，加此判断会保证SDK被初始化1次
+            // 默认的APP会在以包名为默认的process name下运行，如果查到的process name不是APP的process name就立即返回
+
+            if (processAppName == null || !processAppName.equalsIgnoreCase(application.getPackageName())) {
+                // 则此application::onCreate 是被service 调用的，直接返回
+                return false;
+            }
+
+            return true;
+        }
+
+        public static boolean isMainProcess(Context context) {
+            if (context == null) {
+                return false;
+            }
+
+            String packageName = context.getApplicationContext().getPackageName();
+            String processName = getProcessName(context);
+            return packageName.equals(processName);
+        }
+
+        public static String getProcessName(Context context) {
+            String processName = getProcessFromFile();
+            if (processName == null) {
+                // 如果装了xposed一类的框架，上面可能会拿不到，回到遍历迭代的方式
+                processName = getProcessNameByAM(context);
+            }
+            return processName;
+        }
+
+        private static String getProcessFromFile() {
+            BufferedReader reader = null;
+            try {
+                int pid = android.os.Process.myPid();
+                String file = "/proc/" + pid + "/cmdline";
+                reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "iso-8859-1"));
+                int c;
+                StringBuilder processName = new StringBuilder();
+                while ((c = reader.read()) > 0) {
+                    processName.append((char) c);
+                }
+                return processName.toString();
+            } catch (Exception e) {
+                return null;
+            } finally {
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        private static String getProcessNameByAM(Context context) {
+            String processName = null;
+
+            ActivityManager am = ((ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE));
+            if (am == null) {
+                return null;
+            }
+
+            while (true) {
+                List<ActivityManager.RunningAppProcessInfo> plist = am.getRunningAppProcesses();
+                if (plist != null) {
+                    for (ActivityManager.RunningAppProcessInfo info : plist) {
+                        if (info.pid == android.os.Process.myPid()) {
+                            processName = info.processName;
+
+                            break;
+                        }
+                    }
+                }
+
+                if (!TextUtils.isEmpty(processName)) {
+                    return processName;
+                }
+
+                try {
+                    Thread.sleep(100L); // take a rest and again
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        public static boolean isMainProcessLive(Context context) {
+            if (context == null) {
+                return false;
+            }
+
+            final String processName = context.getPackageName();
+            ActivityManager am = ((ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE));
+            if (am != null) {
+                List<ActivityManager.RunningAppProcessInfo> plist = am.getRunningAppProcesses();
+                if (plist != null) {
+                    for (ActivityManager.RunningAppProcessInfo info : plist) {
+                        if (info.processName.equals(processName)) {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /**
+         * 获取进程号对应的进程名
+         *
+         * @param pid 进程号  android.os.Process.myPid()
+         * @return 进程名
+         */
+        private static String getProcessName(int pid) {
+            BufferedReader reader = null;
+            try {
+                reader = new BufferedReader(new FileReader("/proc/" + pid + "/cmdline"));
+                String processName = reader.readLine();
+                if (!TextUtils.isEmpty(processName)) {
+                    processName = processName.trim();
+                }
+                return processName;
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            } finally {
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException exception) {
+                    exception.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        private static String getAppName(Application application, int pID) {
+            String processName = null;
+            ActivityManager am = (ActivityManager) application.getSystemService(Context.ACTIVITY_SERVICE);
+            List l = am.getRunningAppProcesses();
+            Iterator i = l.iterator();
+            PackageManager pm = application.getPackageManager();
+            while (i.hasNext()) {
+                ActivityManager.RunningAppProcessInfo info = (ActivityManager.RunningAppProcessInfo) (i.next());
+                try {
+                    if (info.pid == pID) {
+                        processName = info.processName;
+                        return processName;
+                    }
+                } catch (Exception e) {
+                    // Log.d("Process", "Error>> :"+ e.toString());
+                }
+            }
+            return processName;
+        }
     }
 }
